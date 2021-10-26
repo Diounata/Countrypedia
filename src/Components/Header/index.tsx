80d1e90edbfde3d00ef92349102980cc3da5f641
Diounata <jonathamcordeiro42@gmail.com>
2021-10-26 12:11:52 -0300

    Add: Header link

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import styles from './styles.module.scss';
 
 import MoonIcon from '@icons/Moon';
@@ -6,18 +7,22 @@ import FilledMoonIcon from '@icons/FilledMoon';
 import { useTheme } from '@contexts/ThemeContext';
 
 export default function Header() {
-    const { theme, toggleTheme } = useTheme();
+  const { theme, toggleTheme } = useTheme();
 
-    const icon = { light: <MoonIcon />, dark: <FilledMoonIcon /> };
+  const icon = { light: <MoonIcon />, dark: <FilledMoonIcon /> };
 
-    return (
-        <header className={styles.headerContainer}>
-            <h3>Where in the world?</h3>
+  return (
+    <header className={styles.headerContainer}>
+      <Link href="/">
+        <a>
+          <h3>Where in the world?</h3>
+        </a>
+      </Link>
 
-            <button title="Toggle theme" onClick={toggleTheme}>
-                {icon[theme]}
-                Dark Mode
-            </button>
-        </header>
-    );
+      <button title="Toggle theme" onClick={toggleTheme}>
+        {icon[theme]}
+        Dark Mode
+      </button>
+    </header>
+  );
 }
