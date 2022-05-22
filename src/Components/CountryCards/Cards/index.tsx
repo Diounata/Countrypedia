c47262493216c309ec8b9fdf4eb77e98d9e611cd
Diounata <jonathamcordeiro42@gmail.com>
2022-05-22 11:37:18 -0300

    fix: NaN text

--- src/Components/CountryCards/Cards/index.tsx
+++ src/Components/CountryCards/Cards/index.tsx
@@ -10,8 +10,10 @@ export default function Cards() {
   const { countries, isFiltering } = useCountry();
 
   function showData(value: string | number): string | number {
+    if (typeof value === 'undefined') return 'None';
+
     if (typeof value === 'string') {
-      return value !== '' ? value : 'Unexistent';
+      return value !== '' ? value : 'None';
     }
 
     return new Intl.NumberFormat().format(value);
@@ -20,39 +22,41 @@ export default function Cards() {
   return (
     <>
       <div className={styles.cardsContainer}>
-        {countries.map(({ flag, name, capital, region, population }: CountryCardProps, key: number) => (
-          <div key={key}>
-            <div>
-              <Link href={`/country/${name.toLowerCase()}`}>
-                <a>
-                  <img src={flag} alt={name} title={name} />
-                </a>
-              </Link>
-            </div>
-
-            <div>
-              <Link href={`/country/${name.toLowerCase()}`}>
-                <a>
-                  <h3>{showData(name)}</h3>
-                </a>
-              </Link>
+        {countries.map(
+          ({ flag, name, capital, region, population }: CountryCardProps, key: number) => (
+            <div key={key}>
+              <div>
+                <Link href={`/country/${name.toLowerCase()}`}>
+                  <a>
+                    <img src={flag} alt={name} title={name} />
+                  </a>
+                </Link>
+              </div>
 
               <div>
-                <div>
-                  Population: <span>{showData(population)}</span>
-                </div>
+                <Link href={`/country/${name.toLowerCase()}`}>
+                  <a>
+                    <h3>{showData(name)}</h3>
+                  </a>
+                </Link>
 
                 <div>
-                  Region: <span>{showData(region)}</span>
-                </div>
+                  <div>
+                    Population: <span>{showData(population)}</span>
+                  </div>
 
-                <div>
-                  Capital: <span>{showData(capital)}</span>
+                  <div>
+                    Region: <span>{showData(region)}</span>
+                  </div>
+
+                  <div>
+                    Capital: <span>{showData(capital)}</span>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        ))}
+          )
+        )}
       </div>
 
       {!isFiltering && <Page />}
