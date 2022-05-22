a31ed4fdf2dd321ed6fb1f90ef48893066a37a2e
Diounata <jonathamcordeiro42@gmail.com>
2022-05-22 11:40:44 -0300

    fix: not found country text typo

--- src/Components/CountryCards/NotFound.tsx
+++ src/Components/CountryCards/NotFound.tsx
@@ -1,3 +1,3 @@
 export default function NotFound() {
-  return <div style={{ marginTop: '2rem' }}>This country wasn't find. Please try again</div>;
+  return <div style={{ marginTop: '2rem' }}>This country wasn't found. Please try it again</div>;
 }
